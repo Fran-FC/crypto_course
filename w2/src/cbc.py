1340815d6b95c77016d1b733033e3934fcea3d16
Fran-FC <frafolcm@gmail.com>
2021-11-12 20:26:34 +0100

    updated

--- w2/src/cbc.py
+++ w2/src/cbc.py
@@ -1,7 +1,29 @@
 from Crypto.Cipher import AES
 from Crypto import Random
 
-test_plain_text = "This message is secret bro"
+test_plain_text = "In this segment we're gonna look at another method to achieve chosen plain\
+text security that's actually superior to CBC. And this method is called randomized          \
+counter mode. Unlike CBC. Randomized counter mode uses a secure PRF. It doesn't              \
+need a block cypher. It's enough for counter mode to just use a PRF because                  \
+we're never going to be inverting this function F. So we're going to let F be the            \
+secure PRF and it acts on N byte blocks. Again if we use AES, N will be 128. And             \
+the way the encryption algorithm works in counter mode is it starts off by choosing          \
+a random IV, that's 128 bytes random IV in the case of AES, and the essentially we           \
+start counting. From this random IV, so you notice the first encryption is of IV             \
+then IV+1 up to IV+L. So we generate this random pad. We XOR the result with the             \
+message, and that gives us the cipher text. And, as usual, you notice that the               \
+IV here is included along with the cipher text. So that, in fact, the cipher text is         \
+a little longer than the original plain text. And the point, of course, is that,             \
+encryption algorithm chooses a new IV for every message. And so even if I encrypt            \
+the same message twice, I'm gonna get different resulting cipher texts. One                  \
+thing to notice that this mode is completely paralyzable, unlike CBC. CBC                    \
+was sequential. In other words, you couldn't encrypt block #5 until you've                   \
+encrypted blocks ##1 to 4, so hardware companies who might have multiple                     \
+AES engines working in parallel cannot actually use those AES engines when using             \
+CBC because CBCs inherently sequential. So even though you might have two or three of        \
+four AES engines, you could only use one of them when doing CBC encryption. With             \
+counter mode, everything is completely paralyzable. If you have three AES engines            \
+encryption basically will "
 
 # given hex encoded string keys
 k1 = bytes.fromhex('140b41b22a29beb4061bda66b6747e14')
@@ -68,8 +90,6 @@ def decrypt(cypher, key):
     return m
 
 def main():
-    print(decrypt(c1, k1)) # Basic CBC mode encryption needs padding.
-    print(decrypt(c2, k2)) # Our implementation uses rand. IV
-
+    print(decrypt(encrypt(test_plain_text, k1), k1))
 if __name__ == "__main__":
     main()
